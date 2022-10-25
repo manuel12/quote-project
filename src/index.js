aace988e5f7c1d16ea41b4d48ba6d4ab19b137d2
Manuel <manuelpinedacabeza@gmail.com>
2022-10-25 17:17:48 +0200

    Added codepen App import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import App from './codepen/App';
+
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 
